0d42b4b3d3c4f229ca74344541d64f783bacad06
Vitor Lima <vitor_lima1602@hotmail.com>
2026-08-15 17:39:37 -0300

    chore: initialize business service with PostgreSQL, JPA and Flyway

---
+++ src/main/java/com/estudos/business/repository/TransportadoraRepository.java
@@ -0,0 +1,7 @@
+package com.estudos.business.repository;
+
+import com.estudos.business.entity.Transportadora;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransportadoraRepository extends JpaRepository<Transportadora, Long> {
+}
