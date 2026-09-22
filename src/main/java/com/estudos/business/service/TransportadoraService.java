8d3a7c74acf3669e7253193ca176cf3cafcbe83c
Vitor Lima <vitor_lima1602@hotmail.com>
2026-09-22 20:19:06 -0300

    feat: adiciona tratamento de recurso não encontrado e transações no serviço de transportadoras

--- src/main/java/com/estudos/business/service/TransportadoraService.java
+++ src/main/java/com/estudos/business/service/TransportadoraService.java
@@ -6,11 +6,13 @@ import com.estudos.business.dto.TransportadoraResponse;
 import com.estudos.business.entity.Endereco;
 import com.estudos.business.entity.Transportadora;
 import com.estudos.business.exception.RegraNegocioException;
+import com.estudos.business.exception.TransportadoraNaoEncontradaException;
 import com.estudos.business.mapper.EnderecoMapper;
 import com.estudos.business.mapper.TransportadoraMapper;
 import com.estudos.business.repository.EnderecoRepository;
 import com.estudos.business.repository.TransportadoraRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TransportadoraService {
@@ -27,9 +29,10 @@ public class TransportadoraService {
         this.enderecoMapper = enderecoMapper;
     }
 
+    @Transactional
     public TransportadoraResponse salvar(TransportadoraRequest request){
         if (transportadoraRepository.existsByCnpj(request.getCnpj())){
-            throw  new RegraNegocioException(
+            throw new RegraNegocioException(
                     "Já existe uma transportadora cadastrada com este CNPJ."
             );
         }
@@ -39,8 +42,6 @@ public class TransportadoraService {
         transportadora.setAtiva(true);
 
         Transportadora transportadoraSalva = transportadoraRepository.save(transportadora);
-
-
 
         if (request.getEndereco() != null) {
             Endereco endereco = enderecoMapper.toEntity(request.getEndereco());
@@ -55,20 +56,21 @@ public class TransportadoraService {
         return transportadoraMapper.toResponse(transportadoraSalva);
     }
 
+    @Transactional
     public TransportadoraResponse atualizar(Long id, AtualizarTransportadoraRequest request){
         Transportadora transportadora = buscarPorId(id);
 
         transportadoraMapper.updateEntity(request, transportadora);
 
-        Transportadora transportadoraAtualizada = transportadoraRepository.save(transportadora);
-
-        return transportadoraMapper.toResponse(transportadoraAtualizada);
+        return transportadoraMapper.toResponse(transportadora);
     }
 
+    @Transactional
     public TransportadoraResponse desativar(Long id){
         return alterarStatus(id, false);
     }
 
+    @Transactional
     public TransportadoraResponse ativar(Long id) {
         return alterarStatus(id, true);
     }
@@ -78,16 +80,11 @@ public class TransportadoraService {
 
         transportadora.setAtiva(ativa);
 
-        Transportadora transportadoraAtualizada =
-                transportadoraRepository.save(transportadora);
-
-        return transportadoraMapper.toResponse(transportadoraAtualizada);
+        return transportadoraMapper.toResponse(transportadora);
     }
 
     public Transportadora buscarPorId(Long id){
         return transportadoraRepository.findById(id)
-                .orElseThrow(() -> new RegraNegocioException(
-                        "Transportadora não encontrada."
-                ));
+                .orElseThrow(() -> new TransportadoraNaoEncontradaException(id));
     }
 }
